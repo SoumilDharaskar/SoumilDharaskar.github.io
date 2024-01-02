579471da88db3015d764cd1153759c089a73e5f8
Soumil Dharaskar <soumil.dharaskar@gmail.com>
2024-01-02 05:11:17 -0500

    CICD

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  outDir: './docs',
+  site: 'https://soumildharaskar.github.io',
 });
