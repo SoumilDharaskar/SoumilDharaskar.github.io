85567b89eb84c6c183e4eead8776f393bd2d6a24
Soumil Dharaskar <soumil.dharaskar@gmail.com>
2024-01-02 19:27:38 -0500

    Completed Skills Section added animations to top nav buttons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      animation: {
+        fade: 'fadeIn 1s ease-in-out',
+      },
+
+      keyframes: (theme) => ({
+        fadeIn: {
+          '0%': { backgroundColor: theme('colors.transparent') },
+          '100%': { backgroundColor: theme('colors.red.300') },
+        },
+      }),
+    },
+  },
+  plugins: [],
+};
